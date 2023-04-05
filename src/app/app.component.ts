265e487fa8a372e62e4c5b50b3696a672c8b682c
Eray Bakır <info@eraybakir.com>
2023-04-05 11:14:05 +0300

    Example 1 was finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  getValue() {
-    //JS ile html elementini handle ediyorum
-    let element: any = document.getElementById('name');
-    //element içerisinde ki value değerini handle ediyorum
-    console.log(element.value);
-  }
+  //Bu iki arkadaş form elementlerine bind edildi (Two way binding)
+  creaditCost: number = 10000;
+  installmentCost: number = 1;
+  installmentPerMounth: number = 0;
 
-  //event parametresi içerisinde belirlediğimiz element'e ait tüm özellikleri aldık
-  getEvent(event: any) {
-    console.log(event.target.value);
-  }
-
-  //#nickname in ait olduğu elemente direkt bu şekilde bind işlemi kurabiliyoruz
-  @ViewChild('nickname') nickName: ElementRef<HTMLInputElement>;
-  getNickName() {
-    console.log(this.nickName.nativeElement.value);
-  }
-
-  name:string="Input ile bind edildiği için direkt içerisine yazacak";
-  getNameNgModel(){
-    //ngModel aracılığıyla değeri alabiliyoruz
-    alert(this.name);
+  creditCalculator() {
+    this.installmentPerMounth = this.creaditCost / this.installmentCost;
   }
 }
