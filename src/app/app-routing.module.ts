415ad3dae0f5fcd002eba3fe139a94dc95434bfb
Eray Bakır <info@eraybakir.com>
2023-04-10 00:08:13 +0300

    Spec Test was ignored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { C1Component } from './c1/c1.component';
-import { C2Component } from './c2/c2.component';
+
 
 //Routing'imi de oluşturdum
 const routes: Routes = [
-  {path:"c1",component:C1Component},
-  {path:"c2",component:C2Component}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ColorizeDirective } from './colorize.directive';
-import { MyRouterDirectiveDirective } from './my-router-directive.directive';
-import { C1Component } from './c1/c1.component';
-import { C2Component } from './c2/c2.component';
-import { ValidDirective } from './valid.directive';
-import { SimpleIfDirective } from './simple-if.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ColorizeDirective,
-    MyRouterDirectiveDirective,
-    C1Component,
-    C2Component,
-    ValidDirective,
-    SimpleIfDirective//Direktif'in kullanılabilmesi için ekledim
+    AppComponent
   ],
   imports: [
     BrowserModule,
