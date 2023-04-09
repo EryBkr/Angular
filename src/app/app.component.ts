8383e276ce2aeeaab0987fd2d5c29f9c589574ab
Eray Bakır <info@eraybakir.com>
2023-04-09 12:42:30 +0300

    XOX Game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,63 +6,202 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  //inputtan gelen değeri handle edecek
-  work: string = '';
+  //default olarak X verdik
+  mark: string = 'X';
+  //Bu arkadaşta UI tarafta sıranın kime ait olduğunu belirtecek
+  message: string = 'Sırada ki mark X';
 
-  //will update work
-  updatedWork: string = '';
+  //tahtanın oluşabilmesi için oluşturdum,winner property'si kazanma durumuna göre tahtayı yeşile boyayabilmek için oluşturduğum bir şey
+  games: { mark: string; winner: boolean }[] = [
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+    { mark: '', winner: false },
+  ];
 
-  //Güncelle Formu mu açılacak yoksa ekleme formu mu
-  isActiveUpdateForm: boolean = false;
+  //Bizim için oyun historisini tutacak
+  moves: any = [];
 
-  //Güncellenecek datanın index'i
-  updatedIndex: number = NaN;
+  //Oyun bitti mi?
+  gameOver: boolean = false;
 
-  //işleri ram de kaydedecek
-  works: string[] = [];
+  //Beraberlik ?
+  isMoveFinish: boolean = false;
 
-  //button'un tetikleyeceği func
-  save() {
-    this.works.push(this.work);
-    //work değişkenini string empty'e çeviriyorum ki butona basıldıktan sonra input'un içerisi temizlensin
-    this.work = '';
+  //kutucuğa işaret koymak için
+  setMark(index: number) {
+    //alan işaretli değilse ve oyun bitmediyse setMark işlemi yapılabilsin
+    if (this.games[index].mark == '' && !this.gameOver) {
+      this.games[index].mark = this.mark;
+      //Games içerisinde değeleri sepereate ile value biçimde alıyorum,çünkü games referans tür olduğu için sürekli güncel halini barındıracak fakat biz önceki hallerini de istiyoruz
+      this.moves.push([...this.games]);
+
+      //Her işaretleme işleminden sonra oyunun bitip bitmediğini kontrol ediyorum
+      this.isGameOver();
+
+      //Beraberlik kontrolü
+      this.checkGameIsOver();
+
+      //Oyun bitti ise kazananı message'ye yazıdırıyoruz diğer türlü oyun devam ediyor (ya da beraberlik)
+      if (this.gameOver) this.message = 'Oyun bitti kazanan ' + this.mark;
+      else if (this.isMoveFinish) this.message = 'Oyun berabere';
+      else {
+        //En son hangi işaret koyulmuşsa sonra ki tur tam tersi koyulsun
+        if (this.mark == 'X') this.mark = 'O';
+        else this.mark = 'X';
+
+        //Sıradaki mark'ın kime ait olacağının mesajını oluşturuyoruz
+        this.message = `Sırada ki mark ${this.mark}`;
+      }
+    } else
+      alert(this.gameOver ? 'Oyun Bitti' : 'Lütfen işaretsiz alan seçiniz');
   }
 
-  //sil butonuna basıldığında index'i alıp array den uçuruyoruz
-  remove(index: number) {
-    //confirm tarayıcı tarafından gelen hazır kutucuk modal'ını çalıştırır
-    let result: boolean = confirm('Kaydı Silmek İstediğinize emin misiniz?');
+  //Yeni Oyun butonuna basılırsa
+  newGame() {
+    //Bütün değeleri resetliyorum
+    this.mark = 'X';
+    this.message = 'Sırada ki mark X';
+    this.games = [
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+      { mark: '', winner: false },
+    ];
+    this.gameOver = false;
+    this.moves = [];
+    this.isMoveFinish = false;
+  }
 
-    //Kişi onaylamışsa siliyoruz
-    if (result) {
-      //index'i gelen kaydı sil
-      this.works.splice(index, 1);
+  returnSelectMove(index: number) {
+    //Moves içerisinde games array'lerini barındırıyor (...value olarak aldığımız için referans bağımlılığı yok o an ki değeri neyse array'in onu aldık)
+    //Bu sayede herhangi bir games array'i anına dönebiliyoruz moves=[games,games] gibi bir yapı aslında
+    //games array'ine herhangi bir andaki moves değerini atıyoruz
+    this.games = this.moves[index];
+  }
+
+  //Oyunun bitip bitmediğini kontrol edip kazananı belirliyoruz
+  isGameOver() {
+    if (
+      this.games[0].mark != '' &&
+      this.games[0].mark === this.games[1].mark &&
+      this.games[1].mark === this.games[2].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[0].winner = true;
+      this.games[1].winner = true;
+      this.games[2].winner = true;
+    }
+
+    if (
+      this.games[3].mark != '' &&
+      this.games[3].mark === this.games[4].mark &&
+      this.games[4].mark === this.games[5].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[3].winner = true;
+      this.games[4].winner = true;
+      this.games[5].winner = true;
+    }
+
+    if (
+      this.games[6].mark != '' &&
+      this.games[6].mark === this.games[7].mark &&
+      this.games[7].mark === this.games[8].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[6].winner = true;
+      this.games[7].winner = true;
+      this.games[8].winner = true;
+    }
+
+    if (
+      this.games[0].mark != '' &&
+      this.games[0].mark === this.games[3].mark &&
+      this.games[3].mark === this.games[6].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[0].winner = true;
+      this.games[3].winner = true;
+      this.games[6].winner = true;
+    }
+
+    if (
+      this.games[1].mark != '' &&
+      this.games[1].mark === this.games[4].mark &&
+      this.games[4].mark === this.games[7].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[1].winner = true;
+      this.games[4].winner = true;
+      this.games[7].winner = true;
+    }
+
+    if (
+      this.games[2].mark != '' &&
+      this.games[2].mark === this.games[5].mark &&
+      this.games[5].mark === this.games[8].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[2].winner = true;
+      this.games[5].winner = true;
+      this.games[8].winner = true;
+    }
+
+    if (
+      this.games[0].mark != '' &&
+      this.games[0].mark === this.games[4].mark &&
+      this.games[4].mark === this.games[8].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[0].winner = true;
+      this.games[4].winner = true;
+      this.games[8].winner = true;
+    }
+
+    if (
+      this.games[2].mark != '' &&
+      this.games[2].mark === this.games[4].mark &&
+      this.games[4].mark === this.games[6].mark
+    ) {
+      this.gameOver = true;
+      //Tahta ta işaretli alanları yeşile boyayabilmek için kazanan indexlere winner değerini veriyorum
+      this.games[2].winner = true;
+      this.games[4].winner = true;
+      this.games[6].winner = true;
     }
   }
 
-  //Burada index değeri ve güncellenmiş işin tanımıyla birlikte array de gerekli yeri değiştiriyorum
-  //Ardından güncelleme işlemini bitiriyorum
-  update() {
-    this.works[this.updatedIndex] = this.updatedWork;
-    this.cancelUpdateAction();
+  changeWinnerButtonClass(winner: boolean): string {
+    if (winner) {
+      return 'btn-success';
+    } else return 'btn-warning ';
   }
 
-  //Güncelleme formunun görüntülenmesi için değişkene true set ediyoruz ve güncellenecek datayı handle ediyoruz
-  //index'i de array da ki datayı güncelleyebilmek için aldık
-  viewUpdateForm(willUpdatedwork: string, index: number) {
-    this.updatedIndex = index;
-    this.updatedWork = willUpdatedwork;
-    this.isActiveUpdateForm = true;
-  }
-
-  //Güncelleme işlemi iptal edilsin
-  cancelUpdateAction() {
-    this.isActiveUpdateForm = false;
-  }
-
-  //Input karakterimize göre class ataması yapıyoruz ki daha şekil gözüksün
-  changeInputClassForValidation() {
-    if (this.work.length < 3) return 'is-invalid';
-    return 'is-valid';
+  //Berberlik durumunu kontrol edicek
+  checkGameIsOver() {
+    for (let index = 0; index < this.games.length; index++) {
+      if (this.games[index].mark == '') this.isMoveFinish = false;
+      else {
+        this.isMoveFinish = true;
+      }
+    }
   }
 }
